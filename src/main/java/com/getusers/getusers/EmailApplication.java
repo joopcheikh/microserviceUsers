b1bd9624547774c63f9acb1b54366fb07c335845
sosthene13 <mpsolver01@gmail.com>
2024-09-02 09:24:00 +0000

    get users service

---
+++ src/main/java/com/getusers/getusers/EmailApplication.java
@@ -0,0 +1,16 @@
+package com.getusers.getusers;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class GetUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G.class, args);
+	}
+
+}
+
