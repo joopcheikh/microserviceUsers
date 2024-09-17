5cd68da69117f7baa5d688cfb43786050b1a12f6
sosthene14 <sosthenemounsambote14@gmail.com>
2024-09-17 09:12:55 +0000

    get users

--- src/main/java/com/getusers/getusers/GetUserApplication.java
+++ src/main/java/com/getusers/getusers/GetUserApplication.java
@@ -2,7 +2,6 @@ package com.getusers.getusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
